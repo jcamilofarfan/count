2403f607a10ff96cc8bcf5ea7421d25531013500
jcamilofarfan <farfan1976@hotmail.com>
2022-03-09 11:20:21 -0500

    add config to ORM

--- backend/src/database/database.module.ts
+++ backend/src/database/database.module.ts
@@ -1,4 +1,29 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import config from 'src/config';
 
-@Module({})
+@Global()
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      inject: [config.KEY],
+      useFactory: (configService: ConfigType<typeof config>) => {
+        const { user, host, database, password, port } =
+          configService.postgresDb;
+        return {
+          type: 'postgres',
+          host,
+          port,
+          username: user,
+          password,
+          database,
+          synchronize: true,
+          autoLoadEntities: true,
+        };
+      },
+    }),
+  ],
+  exports: [TypeOrmModule],
+})
 export class DatabaseModule {}
